7281e12dfae2c51dcbde24aad0ff892c99e56b24
Mikael Bonnier <mikael.bonnier@gmail.com>
2016-04-04 02:04:42 +0200

    First commit.

---
+++ src/appinventor/ai_mikael_bonnier/Hippocampus_Game/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"YaVersion":"150","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"19","AppName":"Hippocampus Game","Title":"Hippocampus Game","Uuid":"0","$Components":[{"$Name":"Knapp1","$Type":"Button","$Version":"6","FontSize":"20","Width":"-2","Text":"1","Uuid":"-2052481371"},{"$Name":"Knapp2","$Type":"Button","$Version":"6","FontSize":"20","Width":"-2","Text":"2","Uuid":"-1763165357"},{"$Name":"Knapp3","$Type":"Button","$Version":"6","FontSize":"20","Width":"-2","Text":"3","Uuid":"703304650"},{"$Name":"Knapp4","$Type":"Button","$Version":"6","FontSize":"20","Width":"-2","Text":"4","Uuid":"-1735622815"},{"$Name":"Klocka1","$Type":"Clock","$Version":"3","TimerAlwaysFires":"False","TimerEnabled":"False","TimerInterval":"500","Uuid":"-1180658522"},{"$Name":"Meddelare1","$Type":"Notifier","$Version":"4","Uuid":"119315868"}]}}
+|#
